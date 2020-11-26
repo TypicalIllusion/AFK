825994aa08ae3061936ef04cca0719f934566888
TypicalIllusion <shrimp001x@gmail.com>
2020-11-26 17:03:59 -0500

    Updated version version

--- AFK/AFK.cs
+++ AFK/AFK.cs
@@ -8,7 +8,7 @@ namespace AFK
     {
         public override string Name { get; } = "AFK";
         public override string Author { get; } = "TypicalIllusion";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(2, 0, 0);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 18);
         public override string Prefix { get; } = "AFK";
 
